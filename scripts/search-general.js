f90c52758a06305d9e09a54e0fc6a0df040f5e0c
Melvinz33 <melvin.marley@outlook.fr>
2021-09-22 03:19:27 +0200

    add 1er test algoryhtme reussi

--- scripts/search-general.js
+++ scripts/search-general.js
@@ -8,33 +8,108 @@ export const searchGeneral = async () => {
 	let data = [];
 	let element;
 
-	await app.fetchRecipes();
+	let arraySortByRecipe
 
 
+	let arrayAllDatas = await app.fetchRecipes();
+	console.log(app.bookOfRecipes.recipes)
 
-	if (app.searchTerm.length === 0) {
-		showcards.displayCards(app.bookOfRecipes.recipes)
 
-	} else if (app.searchTerm.length >= 3) {
+	//TRI TABLEAU DANS L 'ORDRE
+	const sortArray = (x,y) => {
+		if (x.name < y.name) {return -1;}
+		if (x.name > y.name) {return 1;}
+		console.log(x)
+	}
 
-		app.bookOfRecipes.recipes
-		.filter((recipe) => {
+	arraySortByRecipe = arrayAllDatas.recipes.sort(sortArray);
 
-			for (element of recipe.ingredients) {
-				if (element.ingredient.toLowerCase().includes(app.searchTerm.toLowerCase())){
-					data.push(recipe)
-				}
+	const val_recherche = "riz";
+
+	const SearchDichotomique= (l, searchTerm) =>{
+	let indiceGauche = 0
+	let indiceDroite = l.length -1;
+	let indiceMilieu
+
+	while (indiceGauche <= indiceDroite){
+
+		indiceMilieu = Math.round((indiceGauche + indiceDroite) / 2)
+
+		let b = l[indiceMilieu].name.indexOf(searchTerm)
+		console.log('cestB' + b)
+
+		if ( b === 10) {
+			console.log("trouvé")
+			return "oki"
+		} else {
+			console.log("algo")
+			let a = new Intl.Collator("fr", {sensitivity: "base"}).compare(l[indiceMilieu].name,searchTerm )
+
+				alert(l[indiceMilieu].name)
+			alert(a)
+			//console.log(val_recherche.localeCompare(searchTerm[indiceMilieu].name, indice'fr', { sensitivity: 'base' }))
+			console.log(a)
+			if  (a == 0){
+			console.log(indiceMilieu)
+			return "finish"
+			} else if ( a  > 0){
+			indiceDroite = indiceMilieu - 1
+
+			} else if (a < 0) {
+			indiceGauche = indiceMilieu +1
+			} else{
+				return "zap"
 			}
+		}
+	}
+}
 
-			if (recipe["name"].toLowerCase().includes(app.searchTerm.toLowerCase())){
-				data.push(recipe)
-			} else if (recipe["description"].toLowerCase().includes(app.searchTerm.toLowerCase())){
-			data.push(recipe)
-			}
-		})
 
-		arrayDeleteElementDuplicate = [...new Set(data)];
-		showcards.displayCards(arrayDeleteElementDuplicate)
-	};
-};
 
+let indice = SearchDichotomique(arraySortByRecipe, val_recherche)
+
+if (indice != 1) {
+	console.log("oki")
+} else {
+	console.log("ko")
+}
+
+
+
+}
+
+
+
+
+
+
+
+
+
+// SEQUENTIEL / DICHOTOMIQUE
+
+//?  CODE QUI MARCHE
+	// if (app.searchTerm.length === 0) {
+	// 	showcards.displayCards(app.bookOfRecipes.recipes)
+
+	// } else if (app.searchTerm.length >= 3) {
+
+	// 	app.bookOfRecipes.recipes
+	// 	.filter((recipe) => {
+
+	// 		for (element of recipe.ingredients) {
+	// 			if (element.ingredient.toLowerCase().includes(app.searchTerm.toLowerCase())){
+	// 				data.push(recipe)
+	// 			}
+	// 		}
+
+	// 		if (recipe["name"].toLowerCase().includes(app.searchTerm.toLowerCase())){
+	// 			data.push(recipe)
+	// 		} else if (recipe["description"].toLowerCase().includes(app.searchTerm.toLowerCase())){
+	// 		data.push(recipe)
+	// 		}
+	// 	})
+
+	// 	arrayDeleteElementDuplicate = [...new Set(data)];
+	// 	showcards.displayCards(arrayDeleteElementDuplicate)
+	// };
